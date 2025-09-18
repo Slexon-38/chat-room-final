2a7157cd018a1aca83c0d16f395994de1cf6b4b7
Slexon-38 <szamarias@gmail.com>
2025-09-19 01:11:38 +0200

    Improve read receipt visibility - change blue checkmarks to cyan for better contrast on dark message background

--- frontend/src/components/ReadReceiptIndicator.jsx
+++ frontend/src/components/ReadReceiptIndicator.jsx
@@ -51,10 +51,10 @@ function ReadReceiptIndicator({ message, currentUsername, onlineUsers }) {
       case 'read':
         return (
           <div className="flex -space-x-1">
-            <svg className="w-4 h-4 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 text-cyan-300" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
-            <svg className="w-4 h-4 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-4 h-4 text-cyan-300" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
             </svg>
           </div>
